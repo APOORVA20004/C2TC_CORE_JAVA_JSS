5ad4c0d4a36c1d0c8f3881b727798c95a4955e0f
APOORVA20004 <akashapoorva1023@gmail.com>
2025-07-31 19:28:51 +0530

    first inherit

---
+++ C2TCPROJECT/src/com/tbsif/inheritence/Multilevel.java
@@ -0,0 +1,17 @@
+package com.tbsif.inheritence;
+
+public class Multilevel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ City ci = new City();
+ ci.setCityName("Bengaluru");
+ ci.setArea("kengeri");
+ ci.setStateName("karnataka");
+ ci.setLanguage("Kannada");
+ ci.setCountryName("INDIA");
+ ci.setCapital("New DELHI");
+ System.out.println(ci);
+	}
+
+}
